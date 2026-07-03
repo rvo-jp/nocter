a835cf743f513e6a270e105ca373988e9d7af337
Rvo JP <chomusuke1003@gmail.com>
2026-07-03 11:49:17 +0900

    プロジェクト再始動

--- src/nocter/int.h
+++
@@ -1,8 +0,0 @@
-#ifndef INT_NOCTER_H
-#define INT_NOCTER_H
-
-#include "../nocter.h"
-
-long conv_int(value val);
-
-#endif
--- src/utils/conv.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_CONV_H
-#define UTILS_CONV_H
-
-#include "../nocter.h"
-
-double conv_float(value val);
-
-#endif
